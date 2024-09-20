1f26aa71af4f63ac9ecf66e87e9e123549f3ee2b
ming-taro <hera002008@naver.com>
2024-09-20 16:12:00 +0900

    move: 데이터 관련 파일 docs 폴더로 이동

--- test/property_for_sale_list.py
+++ test/property_for_sale_list.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 import time
+import pymysql
 
 # 기본 URL
 base_url = "https://www.serve.co.kr/good/v1/map/getAtclList"
@@ -50,27 +51,29 @@ def get_real_estate_list(ldongCd):
     if response.status_code == 200:
         data = response.json()
         if 'data' in data and 'resultList' in data['data']:
-            extract_columns(data['data']['resultList'])
+            extract_columns(ldongCd, data['data']['resultList'])
         else:
             print("No valid data found.")
     else:
         print(f"Error: {response.status_code}")
 
 # 컬럼 추출 함수
-def extract_columns(result_list):
-    extracted_data = []
+def extract_columns(ldongCd, result_list):
+    global total_length
+    # extracted_data = []
 
     for item in result_list:
         # 전체 컬럼
-        sido_nm = item.get('sidoNm')  # 시/도 (서울특별시)
-        sgg_nm = item.get('sggNm')    # 구 (서초구)
-        emd_nm = item.get('emdNm')    # 동 (반포동)
+        # sido_nm = item.get('sidoNm')  # 시/도 (서울특별시)
+        # sgg_nm = item.get('sggNm')    # 구 (서초구)
+        # emd_nm = item.get('emdNm')    # 동 (반포동)
         la_crd = item.get('laCrd')    # 위도 (37.4949233)
         lo_crd = item.get('loCrd')    # 경도 (127.0000303)
         atcl_no = item.get('atclNo')  # 매물 번호 - detail_page 필요 (315343221)
         ctgry_cd1 = item.get('ctgryCd1')  # 주요 카테고리 코드 - detail_page 필요 (SHP)
+        ctgry_cd2 = item.get('ctgryCd2')  # 세부 카테고리 코드
+        ctgry_cd1_nm = item.get('ctgryCd1Nm')  # 주요 카테고리 이름 (상가점포)
         ctgry_cd2_nm = item.get('ctgryCd2Nm')  # 세부 카테고리 이름 (일반상가)
-        bzno = item.get('bzno')  # 사업자 등록번호 (220-23-00073)
         mdiat_bzest_nm = item.get('mdiatBzestNm')  # 중개업체명 (리더스공인중개사사무소)
         deal_kind_cd_nm = item.get('dealKindCdNm')  # 거래 유형 이름 (매매)
         bsc_tnth_wunt_amt = item.get('bscTnthWuntAmt')  # 기본 월세 금액 (90000(만원))
@@ -82,32 +85,45 @@ def extract_columns(result_list):
         photo_list = item.get('photoList', [])
         image_data = photo_list[0].get('imageData') if photo_list else None # 썸네일 (https://newimg.serve.co.kr/article_photo/2024/09/12/14991786/20240912111323289.png)
 
-        extracted_data.append({
-            'sido_nm': sido_nm,  # 시/도 (서울특별시)
-            'sgg_nm': sgg_nm,    # 구 (서초구)
-            'emd_nm': emd_nm,    # 동 (방배동)
-            'la_crd': la_crd,    # 위도 (37.4863705)
-            'lo_crd': lo_crd,    # 경도 (126.9939121)
-            'atcl_no': atcl_no,  # 매물 번호 - detail_page 필요 (315382860)
-            'ctgry_cd1': ctgry_cd1,  # 주요 카테고리 코드 - detail_page 필요 (SHP)
-            'ctgry_cd2_nm': ctgry_cd2_nm,  # 세부 카테고리 이름 (일반상가)
-            'bzno': bzno,  # 사업자 등록번호 (482-07-02306)
-            'mdiat_bzest_nm': mdiat_bzest_nm,  # 중개업체명 (이수자이멘토부동산공인중개사사무소)
-            'deal_kind_cd_nm': deal_kind_cd_nm,  # 거래 유형 이름 (월세)
-            'bsc_tnth_wunt_amt': bsc_tnth_wunt_amt,  # 기본 월세 금액 (4000)
-            'add_tnth_wunt_amt': add_tnth_wunt_amt,  # 추가 월세 금액 (200)
-            'atcl_sfe_cn': atcl_sfe_cn,  # 매물 설명 (렌트프리 적극협의 신축 2년차 상가, 사무실)
-            'atcl_reg_dttm': atcl_reg_dttm,  # 매물 등록일시 (2024-09-13 12:36:57)
-            'area1': area1,  # 면적 (㎡) (113.93)
-            'area2': area2,  # 면적 (㎡) (113.93)
-            'image_data': image_data  # 썸네일
-        })
+        sql = "INSERT INTO property_listing (ldong_cd, atcl_no, atcl_reg_dttm, ctgry_cd1, ctgry_cd1_nm, ctgry_cd2, ctgry_cd2_nm, atcl_sfe_cn, deal_kind_cd_nm, bsc_tnth_wunt_amt, add_tnth_wunt_amt, area1, area2, image_data, la_crd, lo_crd, mdiat_bzest_nm) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        val = (ldongCd, atcl_no, atcl_reg_dttm, ctgry_cd1, ctgry_cd1_nm, ctgry_cd2, ctgry_cd2_nm, atcl_sfe_cn, deal_kind_cd_nm, bsc_tnth_wunt_amt, add_tnth_wunt_amt, area1, area2, image_data, la_crd, lo_crd, mdiat_bzest_nm)
+        try:
+            cursor.execute(sql, val)
+        except Exception as e:
+            print(f"Error occurred: {e}")
 
-    # 추출된 데이터 출력
-    # for row in extracted_data:
-    #     print(row)
-
+        # extracted_data.append({
+        #     'ldong_cd': ldongCd,
+        #     'atcl_no': atcl_no,  # 매물 번호 - detail_page 필요 (315382860)
+        #     'atcl_reg_dttm': atcl_reg_dttm,  # 매물 등록일시 (2024-09-13 12:36:57)
+        #     'ctgry_cd1': ctgry_cd1,  # 주요 카테고리 코드 - detail_page 필요 (SHP)
+        #     'ctgry_cd1_nm': ctgry_cd1_nm,
+        #     'ctgry_cd2': ctgry_cd2,
+        #     'ctgry_cd2_nm': ctgry_cd2_nm,  # 세부 카테고리 이름 (일반상가)
+        #     'atcl_sfe_cn': atcl_sfe_cn,  # 매물 설명 (렌트프리 적극협의 신축 2년차 상가, 사무실)
+        #     'deal_kind_cd_nm': deal_kind_cd_nm,  # 거래 유형 이름 (월세)
+        #     'bsc_tnth_wunt_amt': bsc_tnth_wunt_amt,  # 기본 월세 금액 (4000)
+        #     'add_tnth_wunt_amt': add_tnth_wunt_amt,  # 추가 월세 금액 (200)
+        #     'area1': area1,  # 계약 면적 (㎡) (113.93)
+        #     'area2': area2,  # 전용 면적 (㎡) (113.93)
+        #     'image_data': image_data,  # 썸네일
+        #     'la_crd': la_crd,    # 위도 (37.4863705)
+        #     'lo_crd': lo_crd,    # 경도 (126.9939121)
+        #     'mdiat_bzest_nm': mdiat_bzest_nm,  # 중개업체명 (이수자이멘토부동산공인중개사사무소)
+        # })
+    conn.commit()
     print("결과: ", len(result_list))
+    total_length += len(result_list)
+
+# DB 정보
+conn = pymysql.connect(
+    host='152.69.193.235', 
+    user='employed', 
+    password='employed', 
+    db='KB', 
+    charset='utf8'
+)
+cursor = conn.cursor()
 
 # 거래 종류 선택
 selected_deal_types = ["월세", "매매"]
@@ -118,20 +134,26 @@ offset = 10000
 # 매물 정보 가져오기
 # get_real_estate_list(selected_deal_types)
 
-file_name = 'test\서울시_법정동코드.xls'
+file_name = 'test\docs\서울시_법정동코드.xls'
 
 df = pd.read_excel(file_name)
 
 dong_code_list = []
+total_length = 0
 
 # 데이터 프레임 출력
 for dong_code in df.values:
     dong_code_list.append([dong_code[0],dong_code[1]])
 
 start_time = time.time() # 크롤링 시작
-for dong_code in dong_code_list:
+for dong_code in dong_code_list[188:]:
     print(dong_code[1], end=" ")
     get_real_estate_list(dong_code[0])
+
 end_time = time.time()
 print('total_time')
 print(end_time - start_time) # 73 s
+
+print('총: ', total_length)
+cursor.close()  # 커서 닫기
+conn.close()    # 연결 종료
