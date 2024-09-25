44151b7959a41a8782f6698405963285a88e923a
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-09-25 14:08:18 +0900

    style: 상권정보 UI

--- Frontend/src/pages/InfoPlaza/FloatPopular.vue
+++ Frontend/src/pages/InfoPlaza/FloatPopular.vue
@@ -1,135 +1,40 @@
 <template>
     <div>
-        <InfoPlazaHeader />
+        <InfoPlazaHeader class="mb-5" />
         <div class="container">
             <div class="row">
-                <CommercialHeader class="col-1"/>
-                <div class="col-3">
-                    <div>
+                <CommercialHeader class="ms-10 me-5 col-1"/>
+                <div class="col-3 me-5 mt-3">
+                    <div class="mb-10">
                         <h4 class="mb-3">현위치</h4>
-                        <div class="card border-primary-hover mb-5">
-                            <div class="card-body d-flex gap-3">
-                                <div class="">
-                                    <i class="bi bi-geo-alt w-rem-7 h-rem-7 me-3"></i> <!-- Map icon from Bootstrap Icons -->
-                                    <span class="d-inline text-muted mb-1">서울특별시 광진구 화양동</span>
-                                </div>
-                                <span class="stretched-link ms-auto text-muted text-primary-hover" data-bs-target="#cryptoModal" data-bs-toggle="modal" role="button">
-                                <i class="bi bi-chevron-down"></i>
-                                </span>
+                        <div class=" mb-5">
+                            <div class="card-body card_padding d-flex gap-3">
+                                <i class="bi bi-geo-alt h-rem-7"></i>
+                                <span class="d-inline text-muted">서울특별시 광진구 화양동</span>
                             </div>
                         </div>
                     </div>
                     <div>
                         <h4 class="mb-3">지역 선택</h4>
-                        <div class="card border-primary-hover">
-                            <div class="card-body d-flex gap-3">
-                                <div class="">
-                                    <i class="bi bi-geo-alt w-rem-7 h-rem-7 me-3"></i> <!-- Map icon from Bootstrap Icons -->
-                                    <span class="d-inline text-muted mb-1">서울특별시 광진구 화양동</span>
+                            <div class="card-body card_padding d-flex gap-3">
+                                <div
+                                    class="text-muted text-primary-hover dropdown-toggle"
+                                    id="locationDropdown"
+                                    data-bs-toggle="dropdown"
+                                    role="button"
+                                >
+                                <span class="text-muted pe-100">서울특별시 광진구 화양동</span>
+                                    <ul class="dropdown-menu dropdown-menu-end w-80" aria-labelledby="locationDropdown">
+                                        <li><a class="dropdown-item" href="#">서울특별시 광진구 화양동</a></li>
+                                        <li><a class="dropdown-item" href="#">서울특별시 광진구 화양동</a></li>
+                                        <li><a class="dropdown-item" href="#">서울특별시 광진구 화양동</a></li>
+                                    </ul>
                                 </div>
-                                <span class="stretched-link ms-auto text-muted text-primary-hover" data-bs-target="#cryptoModal" data-bs-toggle="modal" role="button">
-                                <i class="bi bi-chevron-down"></i>
-                                </span>
                             </div>
-                        </div>
                     </div>
                 </div>
-            <div class="col-8" ref="mapContainer" style="height: 70vh;"></div>
+            <div class="col-6 ms-10" ref="mapContainer" style="height: 70vh;"></div>
 
-            <div class="modal fade" id="cryptoModal" tabindex="-1" aria-labelledby="cryptoModalLabel" aria-hidden="true">
-                <div class="modal-dialog modal-dialog-centered">
-                    <div class="modal-content overflow-hidden">
-                        <div class="modal-header pb-0 border-0">
-                        <h1 class="modal-title h4" id="cryptoModalLabel">지역 선택</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body p-0">
-                        <div class="px-6 py-5 border-bottom">
-                            <input type="text" class="form-control" placeholder="검색어를 입력하세요" aria-label="Search">
-                        </div>
-                        <div class="p-2">
-                            <div class="vstack">
-                            <div class="position-relative d-flex gap-3 p-4 rounded bg-body-secondary-hover">
-                                <div class="icon flex-none">
-                                    <i class="bi bi-geo-alt w-rem-7 h-rem-7 mt-1 mx-3 me-3"></i>
-                                </div>
-                                <div class="d-flex flex-fill">
-                                <div class="">
-                                    <a href="#" class="stretched-link text-heading fw-bold">BTC</a>
-                                    <span class="d-block text-muted text-sm">Bitcoin</span>
-                                </div>
-                                <div class="ms-auto fw-bold text-heading">
-                                    23.8
-                                </div>
-                                </div>
-                            </div>
-                            <div class="position-relative d-flex gap-3 p-4 rounded bg-body-secondary-hover">
-                                <div class="icon flex-none">
-                                    <i class="bi bi-geo-alt w-rem-7 h-rem-7 mt-1 mx-3 me-3"></i>
-                                </div>
-                                <div class="d-flex flex-fill">
-                                <div class="">
-                                    <a href="#" class="stretched-link text-heading fw-bold">ETH</a>
-                                    <span class="d-block text-muted text-sm">Ethereum</span>
-                                </div>
-                                <div class="ms-auto fw-bold text-heading">
-                                    1.200,50
-                                </div>
-                                </div>
-                            </div>
-                            <div class="position-relative d-flex gap-3 p-4 rounded bg-body-secondary-hover">
-                                <div class="icon flex-none">
-                                    <i class="bi bi-geo-alt w-rem-7 h-rem-7 mt-1 mx-3 me-3"></i>
-                                </div>
-                                <div class="d-flex flex-fill">
-                                <div class="">
-                                    <a href="#" class="stretched-link text-heading fw-bold">ADA</a>
-                                    <span class="d-block text-muted text-sm">Cardano</span>
-                                </div>
-                                <div class="ms-auto fw-bold text-heading">
-                                    10.930,00
-                                </div>
-                                </div>
-                            </div>
-                            <div class="position-relative d-flex gap-3 p-4 rounded bg-body-secondary-hover">
-                                <div class="icon flex-none">
-                                    <i class="bi bi-geo-alt w-rem-7 h-rem-7 mt-1 mx-3 me-3"></i>
-                                </div>
-                                <div class="d-flex flex-fill">
-                                <div class="">
-                                    <a href="#" class="stretched-link text-heading fw-bold">BNB</a>
-                                    <span class="d-block text-muted text-sm">Binance</span>
-                                </div>
-                                <div class="ms-auto fw-bold text-heading">
-                                    200
-                                </div>
-                                </div>
-                            </div>
-                            <div class="position-relative d-flex gap-3 p-4 rounded bg-body-secondary-hover">
-                                <div class="icon flex-none">
-                                    <i class="bi bi-geo-alt w-rem-7 h-rem-7 mt-1 mx-3 me-3"></i>
-                                </div>
-                                <div class="d-flex flex-fill">
-                                <div class="">
-                                    <a href="#" class="stretched-link text-heading fw-bold">CHAIN</a>
-                                    <span class="d-block text-muted text-sm">Linkchain</span>
-                                </div>
-                                <div class="ms-auto fw-bold text-heading">
-                                    200
-                                </div>
-                                </div>
-                            </div>
-                            </div>
-                        </div>
-                            <div class="px-6 py-5 bg-body-secondary d-flex justify-content-center">
-                                <button class="btn btn-sm btn-dark">
-                                <i class="bi bi-gear me-2"></i>검색
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
         </div>
     </div>
 </div>
@@ -168,4 +73,19 @@ const loadKakaoMap = (container) => {
 
 </script>
 <style>
+.card_padding {
+    padding-top: 3px;
+    padding-bottom: 5px;
+    padding-left: 0px;
+    border: none;
+    box-shadow: none; /* 그림자 제거 */
+    border-bottom: 0.7px solid #D9D9D9; /* 밑에 라인 추가 */
+    border-radius: 0; /* 둥근 테두리 제거 */
+
+}
+
+.pe-100{
+    padding-right: 100px;
+}
+
 </style>
