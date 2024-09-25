52bf99fd4a07b657493f8fde82784807584cdf53
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-09-25 16:23:14 +0900

    style: 가계관리 자산연결 완료 페이지 UI

--- Frontend/src/pages/asset/AssetFin.vue
+++ Frontend/src/pages/asset/AssetFin.vue
@@ -1,9 +1,15 @@
+
 <template>
-  <div>
-    <AssetHeader />
-    <div class="container">
-      <h2>자산 연동이 완료되었습니다.</h2>
-      <router-link to="/asset"><button type="button" class="btn btn-sm btn-primary mb-5 mt-5 col-12 m-0">가계관리로 돌아가기</button></router-link>
+  <div class="">
+    <AssetHeader class="mb-12"/>
+    <div class="container text-center">
+      <div class="mb-12">
+      <h2 class="mb-4 fw-bold">자산 연동이 완료되었습니다.</h2>
+      <img src="@/assets/img/asset/assetfin.png" class="rounded-circle mb-4 asset-image" />
+    </div>
+      <router-link to="/asset">
+        <button type="button" class="btn btn-sm btn-primary mb-5 mt-5 col-12 m-0">가계관리로 돌아가기</button>
+      </router-link>
     </div>
   </div>
 </template>
@@ -13,18 +19,9 @@ import AssetHeader from '@/components/asset/AssetHeader.vue';
 </script>
 
 <style>
-.btn-primary {
-  margin: 0px 0px 0px 20px;
-}
-.btn-cls {
-  display: flex;
-  width: 18%;
-  padding: 0.5% 0% 0% 20%;
-}
-.div-input {
-  width: 40%;
-}
-.gray{
-  color: gray;
+.asset-image {
+  width: 150px; /* Adjust the size as needed */
+  height: 150px; /* Adjust the size as needed */
+  max-width: 100%;
 }
 </style>
