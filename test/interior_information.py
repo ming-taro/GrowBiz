fc217966df1f47e359ebebbac53fd59d7295f5f5
ming-taro <hera002008@naver.com>
2024-09-25 17:46:52 +0900

    feat: 창업비용 데이터 추가

--- test/interior_information.py
+++ test/interior_information.py
@@ -15,14 +15,14 @@ API_KEY = os.environ.get('API_KEY')
 
 
 # 인테리어 비용
-def get_interior_cost(brand_info, brand_mnno, year):
+def find_interior_cost(brand_info, year):
     brand_frcs_bzm_intrrctinfo_params = {
         "serviceKey": API_KEY,
         "pageNo": 1,
         "numOfRows": 1,
         "resultType": "json",
         "jngBizCrtraYr": year,
-        "brandMnno": brand_mnno
+        "brandMnno": brand_info['brand_mnno']
     }
 
     response = requests.get(BRAND_FRCS_BZM_INTRRCTINFO_URL, params=brand_frcs_bzm_intrrctinfo_params)
@@ -76,7 +76,7 @@ def get_brand_frc_bzmn_avrgsls(year, brand_mnno):
 
 
 # 브랜드별 창업 비용
-def get_brand_fntn_stats(year):
+def get_brand_fntn_stats(brand_fntn_info, year):
     brand_fntn_stats_params = {
         "serviceKey": API_KEY,
         "pageNo": 1,
@@ -87,7 +87,7 @@ def get_brand_fntn_stats(year):
 
     response = requests.get(FFTC_BRAND_FNTN_STATS_URL, params=brand_fntn_stats_params)
     total_count = response.json()["totalCount"]
-    print(total_count)
+    print("total_count:", total_count)
     
     for i in range(1, int(total_count / 100 + 2)): # 100개씩 저장
         brand_fntn_stats_params = {
@@ -103,27 +103,22 @@ def get_brand_fntn_stats(year):
             data = response.json()
             items = data["items"]
             
-            brand_fntn_info = []
-
             for item in items:
-                brand_fntn_info.append({
-                    "induty_lclas_nm": item['indutyLclasNm'],            # 업종 대분류 이름 (외식)
-                    "induty_mlsfc_nm": item['indutyMlsfcNm'],            # 업종 중분류 이름 (한식)
-                    "brand_nm": item['brandNm'],                         # 브랜드 이름 (두돈까스)
-                    "corp_nm": item['corpNm'],                           # 회사 이름 ((주)아이언가이즈)
-                    "jng_bzmn_jng_amt": item['jngBzmnJngAmt'],           # 가맹 사업자 가맹 금액 (3300)
-                    "jng_bzmn_edu_amt": item['jngBzmnEduAmt'],           # 가맹 사업자 교육비 (2200)
-                    "jng_bzmn_assrnc_amt": item['jngBzmnAssrncAmt'],     # 가맹 사업자 보증금 (1000)
-                    "jng_bzmn_etc_amt": item['jngBzmnEtcAmt'],           # 가맹 사업자 기타 금액 (29133)
-                    "smtn_amt": item['smtnAmt']                          # 합계 금액 (35633)
-                })
+                if item['brandNm'] not in brand_fntn_info:
+                    brand_fntn_info[item['brandNm']] = {
+                        "jng_bzmn_jng_amt": item['jngBzmnJngAmt'],           # 가맹 사업자 가맹 금액 (3300)
+                        "jng_bzmn_edu_amt": item['jngBzmnEduAmt'],           # 가맹 사업자 교육비 (2200)
+                        "jng_bzmn_assrnc_amt": item['jngBzmnAssrncAmt'],     # 가맹 사업자 보증금 (1000)
+                        "jng_bzmn_etc_amt": item['jngBzmnEtcAmt'],           # 가맹 사업자 기타 금액 (29133)
+                        "smtn_amt": item['smtnAmt']                          # 합계 금액 (35633)
+                    }
         except requests.exceptions.RequestException as e:
             print(f"Error: {e}")
     return brand_fntn_info
 
 
 # 브랜드 정보
-def get_brand_info(year):
+def get_brand_info(brand_fntn_info, year):
     brand_info_params = {
         "serviceKey": API_KEY,
         "pageNo": 1,
@@ -155,38 +150,37 @@ def get_brand_info(year):
             brand_info = []
 
             for item in items:
-                brandMnno = item['brandMnno']          # 브랜드 모델 번호
-                jnghdqrtrsRprsvNm = item['jnghdqrtrsRprsvNm']  # 본사 대표 이름
-                brandNm = item['brandNm']              # 브랜드 이름
-                indutyLclasNm = item['indutyLclasNm']  # 산업 대분류 이름
-                indutyMlsfcNm = item['indutyMlsfcNm']  # 산업 중분류 이름
-                majrGdsNm = item['majrGdsNm']          # 주요 상품명
-
                 brand_info_entry = {
-                    "brand_mnno": brandMnno,                   # 브랜드 모델 번호
-                    "jnghdqrtrs_rprsv_nm": jnghdqrtrsRprsvNm,  # 본사 대표 이름
-                    "brand_nm": brandNm,                       # 브랜드 이름
-                    "induty_lclas_nm": indutyLclasNm,          # 산업 대분류 이름
-                    "induty_mlsfc_nm": indutyMlsfcNm,          # 산업 중분류 이름
-                    "majr_gds_nm": majrGdsNm,                  # 주요 상품명
-                    "fyer_avrg_sls_amt_scope_val": get_brand_frc_bzmn_avrgsls(year, brandMnno) # 연평균 매출 범위
+                    "brand_mnno": item['brandMnno'],                   # 브랜드 모델 번호
+                    "jnghdqrtrs_rprsv_nm": item['jnghdqrtrsRprsvNm'],  # 본사 대표 이름
+                    "brand_nm": item['brandNm'],                       # 브랜드 이름
+                    "induty_lclas_nm": item['indutyLclasNm'],          # 산업 대분류 이름
+                    "induty_mlsfc_nm": item['indutyMlsfcNm'],          # 산업 중분류 이름
+                    "majr_gds_nm": item['majrGdsNm'],                  # 주요 상품명
+                    "fyer_avrg_sls_amt_scope_val": get_brand_frc_bzmn_avrgsls(year, item['brandMnno']) # 연평균 매출 범위
                 }
 
-                get_interior_cost(brand_info_entry, brandMnno, year)
+                find_interior_cost(brand_info_entry, year) # 인테리어 비용
+
+                if brand_info_entry['brand_nm'] in brand_fntn_info: # 창업비용
+                    # print("========찾음======= 브랜드명 ->", brand_info_entry['brand_nm'])
+                    for key, value in brand_fntn_info[brand_info_entry['brand_nm']].items():
+                        brand_info_entry[key] = value
+                    # print(json.dumps(brand_info_entry, ensure_ascii=False, indent=4)) # json 형식으로 출력
+                    # print("===================")
+
                 brand_info.append(brand_info_entry)
-                print(json.dumps(brand_info_entry, ensure_ascii=False, indent=4)) # json 형식으로 출력
         except requests.exceptions.RequestException as e:
             print(f"Error: {e}")
     return brand_info
 
-get_brand_fntn_stats(2023)
+brand_fntn_info = {}
+for year in range(datetime.now().year - 1, 2017, -1): # 창원
+    print(year)
+    get_brand_fntn_stats(brand_fntn_info, year)
+    print("brand_fntn_info 길이:", len(brand_fntn_info))
 
 result = []
-current_year = datetime.now().year
 year = 2023
 # for year in years:
-result.extend(get_brand_info(2023))
-print("total:", len(result))
-
-# for brand in result:
-#     print(json.dumps(brand, ensure_ascii=False, indent=4)) # json 형식으로 출력
+result.extend(get_brand_info(brand_fntn_info, year))
