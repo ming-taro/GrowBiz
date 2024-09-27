2ddb445b4e28cff002ce5c9b1aa8169eeaffa491
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-09-27 10:26:28 +0900

    feat: 메인 대출정보 컴포넌트 추가, 정보광장 수정

--- Frontend/src/components/main/MainInfoPlaza.vue
+++ Frontend/src/components/main/MainInfoPlaza.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="container mb-5">
-        <h2 class="fw-bold mb-5">정보광장</h2>
+<div class="container mb-5">
+    <h2 class="fw-bold mb-5">정보광장</h2>
         <hr/>
-        <div class="content-wrapper">
+        <div class="content-wrapper  mb-10">
             <div class="adjusted-float-popular">
                 <FloatPopularContent :showLocationInfo="false" />
             </div>
@@ -11,41 +11,46 @@
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-6">
-        <MainEducation/>
-    </div></div>
+    <div class="row">
+        <div class="col-6">
+            <MainEducation/>
+        </div>
+        <div class="col-6">
+            <MainLoan/>
+        </div>
     </div>
+</div>
 </template>
 
 <script setup>
 import FloatPopularContent from '@/components/infoplaza/FloatPopularContent.vue';
-import MainPopularIndustry from '@/components/main/MainPopularIndustry.vue';
 import MainEducation from '@/components/main/MainEducation.vue';
+import MainLoan from '@/components/main/MainLoan.vue';
+import MainPopularIndustry from '@/components/main/MainPopularIndustry.vue';
 </script>
 
 <style>
 .content-wrapper {
-    display: flex;
-    justify-content: space-between;
+display: flex;
+justify-content: space-between;
 }
 
 .adjusted-float-popular {
-    flex: 2; /* 필요한 크기로 조정 */
+flex: 2; /* 필요한 크기로 조정 */
 }
 
 .adjusted-main-popular {
-    flex: 1; /* 필요한 크기로 조정 */
+flex: 1; /* 필요한 크기로 조정 */
 }
 
 /* col-1, col-6에 대한 스타일 재정의 */
 .adjusted-float-popular .col-1 {
-    flex: 0 0 15%; /* col-1을 20%로 설정 */
-    max-width: 15%;
+flex: 0 0 15%; /* col-1을 20%로 설정 */
+max-width: 15%;
 }
 
 .adjusted-float-popular .col-6 {
-    flex: 0 0 60%; /* col-6을 50%로 설정 */
-    max-width: 60%;
+flex: 0 0 60%; /* col-6을 50%로 설정 */
+max-width: 60%;
 }
 </style>
