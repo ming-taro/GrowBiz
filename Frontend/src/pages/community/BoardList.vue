a12967a110678ceaff6d7cc9fae975cb28733a62
임종민 <dlagudrb78@gmail.com>
2024-09-27 13:07:05 +0900

    remove: simulcopy 삭제

--- Frontend/src/pages/community/BoardList.vue
+++ Frontend/src/pages/community/BoardList.vue
@@ -1,42 +1,42 @@
 <template>
-<div class="container">
+  <div class="container">
     <div class="border-top">
-    <div class="table-responsive">
+      <div class="table-responsive">
         <table class="table table-nowrap text-center">
-        <thead>
+          <thead>
             <tr>
-            <th scope="col">번호</th>
-            <th scope="col">제목</th>
-            <th scope="col">작성자</th>
-            <th scope="col">조회</th>
-            <th scope="col">추천</th>
-            <th scope="col">작성일</th>
+              <th scope="col">번호</th>
+              <th scope="col">제목</th>
+              <th scope="col">작성자</th>
+              <th scope="col">조회</th>
+              <th scope="col">추천</th>
+              <th scope="col">작성일</th>
             </tr>
-        </thead>
-        <tbody>
+          </thead>
+          <tbody>
             <tr v-for="post in posts" :key="post.id">
-            <td>{{ post.id }}</td>
-            <td>
+              <td>{{ post.id }}</td>
+              <td>
                 <a :href="'community/view/' + post.id">{{ post.title }}</a>
-            </td>
-            <td>{{ post.author }}</td>
-            <td>{{ post.views }}</td>
-            <td>{{ post.likes }}</td>
-            <td>{{ post.date }}</td>
+              </td>
+              <td>{{ post.author }}</td>
+              <td>{{ post.views }}</td>
+              <td>{{ post.likes }}</td>
+              <td>{{ post.date }}</td>
             </tr>
-        </tbody>
+          </tbody>
         </table>
-    </div>
-    <div class="py-4 px-6">
+      </div>
+      <div class="py-4 px-6">
         <div class="row align-items-center justify-content-between">
-        <div class="col-md-6 d-none d-md-block"></div>
-        <div class="col-md-auto">
+          <div class="col-md-6 d-none d-md-block"></div>
+          <div class="col-md-auto">
             <nav aria-label="Page navigation example">
-            <ul class="pagination pagination-spaced gap-1">
+              <ul class="pagination pagination-spaced gap-1">
                 <li class="page-item">
-                <a class="page-link" href="#">
+                  <a class="page-link" href="#">
                     <i class="bi bi-chevron-left"></i>
-                </a>
+                  </a>
                 </li>
                 <li class="page-item"><a class="page-link" href="#">1</a></li>
                 <li class="page-item"><a class="page-link" href="#">2</a></li>
@@ -44,31 +44,83 @@
                 <li class="page-item"><a class="page-link" href="#">4</a></li>
                 <li class="page-item"><a class="page-link" href="#">5</a></li>
                 <li class="page-item">
-                <a class="page-link" href="#">
+                  <a class="page-link" href="#">
                     <i class="bi bi-chevron-right"></i>
-                </a>
+                  </a>
                 </li>
                 <a href="community/create">
-                <button type="button" class="btn btn-sm btn-neutral">작성</button>
+                  <button type="button" class="btn btn-sm btn-neutral">
+                    작성
+                  </button>
                 </a>
-            </ul>
+              </ul>
             </nav>
+          </div>
         </div>
+      </div>
+      <div class="row justify-content-center mb-10">
+        <div class="col-9">
+          <div class="mt-2">
+            <form class="form-group">
+              <div class="input-group input-group-sm">
+                <div class="btn-group dropdown">
+                  <select
+                    class="form-select"
+                    aria-label="Default select example"
+                  >
+                    <option selected>전체</option>
+                    <option value="1">과정명</option>
+                    <option value="2">내용</option>
+                    <option value="3">해시태그</option>
+                    <option value="4">강사명</option>
+                  </select>
+                </div>
+                <input
+                  type="text"
+                  class="form-control ms-3"
+                  placeholder="Search query..."
+                />
+                <span class="input-group-text">
+                  <i class="bi bi-search"></i>
+                </span>
+              </div>
+            </form>
+          </div>
         </div>
+      </div>
     </div>
-    </div>
-</div>
+  </div>
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
 const posts = ref([
-{ id: 3, title: '제목공간제목공간', author: '누구1', views: 1234, likes: 3, date: '2024-07-28' },
-{ id: 2, title: '제목공간제목공간', author: '누구2', views: 1234, likes: 2, date: '2024-07-29' },
-{ id: 1, title: '제목공간제목공간제목', author: '누구3', views: 1234, likes: 5, date: '2024-08-29' },
+  {
+    id: 3,
+    title: '제목공간제목공간',
+    author: '누구1',
+    views: 1234,
+    likes: 3,
+    date: '2024-07-28',
+  },
+  {
+    id: 2,
+    title: '제목공간제목공간',
+    author: '누구2',
+    views: 1234,
+    likes: 2,
+    date: '2024-07-29',
+  },
+  {
+    id: 1,
+    title: '제목공간제목공간제목',
+    author: '누구3',
+    views: 1234,
+    likes: 5,
+    date: '2024-08-29',
+  },
 ]);
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
