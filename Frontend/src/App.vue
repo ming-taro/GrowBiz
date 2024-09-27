d95c76ad3231c0bbb169c81a990c4e8c09346a11
임종민 <dlagudrb78@gmail.com>
2024-09-27 15:39:17 +0900

    feat: 폰트 추가

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -9,8 +9,20 @@
 <script setup>
 import Header from '@/components/common/Header.vue';
 import Footer from '@/components/common/Footer.vue';
+import './assets/css/fonts.css';
 </script>
 
 <style>
 @import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css');
+
+@font-face {
+  font-family: 'Pretendard-Black';
+  src: url('@/assets/fonts/Pretendard-Black.otf') format('opentype');
+  font-weight: 900;
+  font-style: normal;
+}
+
+body {
+  font-family: 'Pretendard-Black', sans-serif;
+}
 </style>
