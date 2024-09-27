668a18d1c879cdb6022aef2afec86b213a4de5a1
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-09-27 17:47:12 +0900

    style: 메인 가게정보 UI 수정

--- Frontend/src/components/main/MainMyStore.vue
+++ Frontend/src/components/main/MainMyStore.vue
@@ -12,14 +12,12 @@
         <!-- 왼쪽 카드의 부모 -->
         <div class="card image-card d-flex flex-column">
         <!-- 카드가 세로로 늘어날 수 있도록 설정 -->
-        <div class="card-body pb-5 flex-grow-1">
             <div class="d-flex justify-content-between align-items-center">
             <img
                 src="https://www.jumpoline.com/_file/jumpo/jumpoline_202406121654211156.jpg"
                 class="card-img-top rounded"
             />
             </div>
-        </div>
         </div>
     </div>
 
@@ -31,40 +29,40 @@
             <div class="">
             <div class="d-flex justify-content-between gap-4">
                 <div class="">
-                <span class="d-block text-sm text-heading fw-bold"
+                <span class="d-block text-md text-heading fw-bold"
                     >권리금</span
                 >
-                <span class="d-block text-sm text-heading fw-bold"
+                <span class="d-block text-md text-heading fw-bold"
                     >총 투자액</span
                 >
-                <span class="d-block text-sm text-heading fw-bold"
+                <span class="d-block text-md text-heading fw-bold"
                     >월매출</span
                 >
-                <span class="d-block text-sm text-heading fw-bold"
+                <span class="d-block text-md text-heading fw-bold"
                     >마진율</span
                 >
-                <span class="d-block text-sm text-heading fw-bold"
+                <span class="d-block text-md text-heading fw-bold"
                     >매출이익</span
                 >
-                <span class="d-block text-sm text-heading fw-bold"
+                <span class="d-block text-md text-heading fw-bold"
                     >경비 합계</span
                 >
-                <span class="d-block text-sm text-heading fw-bold"
+                <span class="d-block text-md text-heading fw-bold"
                     >월수익</span
                 >
-                <span class="d-block text-sm text-heading fw-bold"
+                <span class="d-block text-md text-heading fw-bold"
                     >권리금 회수기간</span
                 >
                 </div>
                 <div class="text-end">
-                <span class="d-block text-sm text-heading fw-bold">100원</span>
-                <span class="d-block text-sm text-heading fw-bold">100원</span>
-                <span class="d-block text-sm text-heading fw-bold">100원</span>
-                <span class="d-block text-sm text-heading fw-bold">100원</span>
-                <span class="d-block text-sm text-heading fw-bold">100원</span>
-                <span class="d-block text-sm text-heading fw-bold">100원</span>
-                <span class="d-block text-sm text-heading fw-bold">100원</span>
-                <span class="d-block text-sm text-heading fw-bold">1일</span>
+                <span class="d-block text-md text-heading fw-bold">100원</span>
+                <span class="d-block text-md text-heading fw-bold">100원</span>
+                <span class="d-block text-md text-heading fw-bold">100원</span>
+                <span class="d-block text-md text-heading fw-bold">100원</span>
+                <span class="d-block text-md text-heading fw-bold">100원</span>
+                <span class="d-block text-md text-heading fw-bold">100원</span>
+                <span class="d-block text-md text-heading fw-bold">100원</span>
+                <span class="d-block text-md text-heading fw-bold">1일</span>
                 </div>
             </div>
             </div>
