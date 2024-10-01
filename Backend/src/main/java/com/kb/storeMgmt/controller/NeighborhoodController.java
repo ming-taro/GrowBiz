6affbbe640dc1194639106d20eefb5b0f3eadabe
임종민 <dlagudrb78@gmail.com>
2024-10-01 17:07:39 +0900

    fix: 주소 입력 폼 만들기 전으로 복구

--- Backend/src/main/java/com/kb/storeMgmt/controller/NeighborhoodController.java
+++ Backend/src/main/java/com/kb/storeMgmt/controller/NeighborhoodController.java
@@ -1,4 +1,5 @@
-package com.kb.storeMgmt.controller;
+
+        package com.kb.storeMgmt.controller;
 
 import com.kb.storeMgmt.service.NeighborhoodService;
 import lombok.RequiredArgsConstructor;
@@ -6,7 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+        import java.util.Map;
 
 @RestController
 @RequestMapping("/api/neighborhoods")
