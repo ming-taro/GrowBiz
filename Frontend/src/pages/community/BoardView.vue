905e82638a8778866c08d138ea9155bc3b66eae6
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-10-07 16:10:40 +0900

    style: 커뮤니티 댓글 추가

--- Frontend/src/pages/community/BoardView.vue
+++ Frontend/src/pages/community/BoardView.vue
@@ -9,6 +9,36 @@
         <button type="button" class="btn btn-sm btn-neutral mx-1">👎 비추천</button>
       </div>
       <RouterLink :to="`/community/${category}`" class="btn btn-sm btn-neutral mb-5 mt-1">목록</RouterLink>
+
+      
+      <div class="list-group mt-5 mb-5">
+        <h4 class="fw-semibold mb-5">댓글 {{ comments.length }}</h4>
+        <div class="list-group-item py-3" v-for="comment in comments" :key="comment.author">
+          <div class="d-flex flex-wrap w-100 justify-content-between py-2">
+            <h6>{{ comment.author }}</h6>
+          </div>
+          <p class="fs-sm font-weight-normal text-body py-2">{{ comment.content }}</p>
+          <small class="text-muted">{{ comment.date }}</small>
+        </div>
+      </div>
+
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center pagination-spaced gap-1">
+          <li class="page-item">
+            <a class="page-link" href="#">
+              <i class="bi bi-chevron-left"></i>
+            </a>
+          </li>
+          <li class="page-item" v-for="page in totalPages" :key="page">
+            <a class="page-link" href="#">{{ page }}</a>
+          </li>
+          <li class="page-item">
+            <a class="page-link" href="#">
+              <i class="bi bi-chevron-right"></i>
+            </a>
+          </li>
+        </ul>
+      </nav>
     </div>
   </div>
 </template>
@@ -36,7 +66,13 @@ const fetchPost = async () => {
     console.error('게시글을 가져오는 데 실패했습니다:', error);
   }
 };
+const comments = ref([
+  { author: '작성자1', content: '댓글댓글댓글댓글댓글...', date: '2014-01-23' },
+  { author: '작성자2', content: '댓글댓글댓글댓글...', date: '2015-01-25' },
+  { author: '작성자3', content: '댓글댓글댓글댓글...', date: '2017-03-23' },
+]);
 
+const totalPages = ref(5); // 페이지네이션
 </script>
 
 <style scoped>
