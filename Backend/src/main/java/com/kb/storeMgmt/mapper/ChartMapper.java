fd63494eddada996bd454500f1d7ba1af261bcf0
임종민 <dlagudrb78@gmail.com>
2024-10-07 18:05:18 +0900

    feat: 가게관리 상단 그래프 완료

--- Backend/src/main/java/com/kb/storeMgmt/mapper/ChartMapper.java
+++ Backend/src/main/java/com/kb/storeMgmt/mapper/ChartMapper.java
@@ -1,7 +1,13 @@
 package com.kb.storeMgmt.mapper;
 
-import com.kb.storeMgmt.dto.FlowPopulationDTO;
+import com.kb.storeMgmt.dto.CategoriesDTO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 public interface ChartMapper {
-    public FlowPopulationDTO getFlpopCobyDongname(String dongname);
+    public List<CategoriesDTO> getDoughnut(CategoriesDTO categoriesDTO);
+
+    List<CategoriesDTO> getMixAddress(CategoriesDTO categoriesDTO);
+
 }
---
+++ Frontend/src/components/asset/MyMapGraph.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div style="height: 400px">
+      <div class="he-half">
+        <div>
+          <h4 class="mb-2">주변 상권 별 매출 데이터 비교</h4>
+        </div>
+        <div>
+          <canvas id="doughnut-chart2" style="height: 202px"></canvas>
+        </div>
+      </div>
+      <div>
+        <div class="he-half">
+          <h4 class="">대분류 별 월매출 비교</h4>
+        </div>
+        <div>
+          <canvas id="mixed-chart2" height="100px"></canvas>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<!-- 
+<script setup>
+import { onMounted, ref } from 'vue';
+import { Chart } from 'chart.js/auto';
+import {
+  asset_data_doughnut,
+  asset_doughnutoptions,
+  mixed_data,
+  mixed_options,
+  fetchChartData,
+} from '@/assets/js/assetChart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+Chart.register(ChartDataLabels);
+
+let doughnutChartInstance = ref(null); // 도넛 차트 인스턴스
+let mixedChartInstance = ref(null); // 혼합 차트 인스턴스
+let loanRepaymentStatus = ref(''); // 대출 상환 현황 상태 추가
+
+onMounted(async () => {
+  const asdf = await fetchChartData(loanRepaymentStatus); // loanRepaymentStatus를 매개변수로 전달
+
+  const doughnutCtx = document
+    .getElementById('doughnut-chart2')
+    .getContext('2d');
+  const mixedCtx = document.getElementById('mixed-chart2').getContext('2d');
+
+  // Chart.js를 사용하여 도넛 차트 생성
+  doughnutChartInstance.value = new Chart(doughnutCtx, {
+    type: 'pie',
+    data: asset_data_doughnut,
+    options: {
+      ...asset_doughnutoptions, // asset_doughnutoptions의 모든 속성을 가져옴
+      maintainAspectRatio: false, // 이 속성을 덮어쓰는 방식으로 추가
+    },
+  });
+
+  // Chart.js를 사용하여 혼합 차트 생성
+  mixedChartInstance.value = new Chart(mixedCtx, {
+    type: 'bar', // 기본 차트 유형은 'bar'입니다. 혼합 차트 데이터셋에 따라 조정됩니다.
+    data: mixed_data,
+    options: mixed_options,
+    maintainAspectRatio: false,
+  });
+});
+</script>
+
+<style scoped>
+.he-half {
+  width: 80%;
+  height: 60%;
+}
+</style> -->
