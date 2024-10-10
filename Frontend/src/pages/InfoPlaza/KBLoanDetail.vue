bc869208106f64f70788c8a780577b612d16699e
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-10-10 14:29:53 +0900

    fix: KB 디테일 데이터 매칭

--- Frontend/src/pages/InfoPlaza/KBLoanDetail.vue
+++ Frontend/src/pages/InfoPlaza/KBLoanDetail.vue
@@ -1,77 +1,255 @@
 <template>
   <div>
     <InfoPlazaHeader />
-    <div class="container mw-screen-xl">
+    <div class="custom-container">
       <div class="row">
-        <!-- 데이터가 있을 때만 렌더링 -->
+        <!-- Signle job content-->
+        <div
+          class="col-lg-6 position-relative pe-0 me-10 mb-5 mb-lg-0"
+          style="z-index: 1025"
+        >
         <div v-if="data">
-          <!-- Product details-->
-          <div class="offset-xl-1">
-            <h1>{{ data.productName }}</h1>
-            <hr />
-            <div class="row">
-              <div class="col-4 d-flex align-items-center justify-content-center">
-                <span>
-                  <i class="fa-solid fa-calendar-days" style="font-size: 50px"></i>
-                </span>
-                <span class="text ms-3">
-                  <span style="color: gray">신청기간</span> <br />
-                  <strong>{{ data.applicationPeriod }}</strong>
-                </span>
+          <div class="d-flex justify-content-between mb-2">
+            <div class="d-flex align-items-center">
+              <div class="d-flex align-items-center me-5">
+                <img
+                  :src="'/images/banklogo/국민은행.png'"
+                  alt=""
+                  style="width: 50px"
+                />
               </div>
-              <div class="col-4 d-flex align-items-center justify-content-center">
-                <span>
-                  <i class="fa-solid fa-coins" style="font-size: 50px"></i>
-                </span>
-                <span class="text ms-3">
-                  <span style="color: gray">금리</span> <br />
-                  <strong style="color: brown">{{ data.totalInterestRate }}</strong>
-                </span>
-              </div>
-              <div class="col-4 d-flex align-items-center justify-content-center">
-                <span>
-                  <i class="fa-solid fa-won-sign" style="font-size: 50px"></i>
-                </span>
-                <span class="text ms-3">
-                  <span style="color: gray">가산금리</span> <br />
-                  <strong style="color: brown">{{ data.additionalInterestRate }}</strong>
-                </span>
+              <div class="d-flex align-items-center">
+                <div class="text-left">
+                  <span class="fs-2 text-gray-500 fw-bolder d-block">
+                    {{ data.productName }}
+                  </span>
+                </div>
               </div>
             </div>
-            <hr />
-            <div class="mb-5">
-              <h4>
-              자격 요건
-            </h4> 
-            <span v-html="formattedLoanEligibility"></span> <!-- 줄 바꿈 적용 -->
+            
           </div>
-
-            <div class="mb-5">
-              <h4>
+          <ul class="list-unstyled fs-sm ms-1 mb-4">
+            <li class="mb-2">
+              <i class="fa-solid fa-house-user text-muted me-2"></i
+              ><span>KB국민은행</span>
+            </li>
+            
+            <li class="d-flex align-items-center mb-2">
+              <i class="fa-solid fa-coins text-muted me-2">                     
+                </i
+              ><span>
+                {{ data.lowestInterestRate ? data.lowestInterestRate + '%' : '변동' }}
+              </span>
+            </li>
+          </ul>
+          <hr class="mb-4" />
+          <ul class="list-unstyled">
+            <li class="mb-4">
+              <h4 class="mb-1">
+                자격 요건
+              </h4> 
+              <span v-if="data.loanEligibility" v-html="formattedLoanEligibility"></span>
+              <span v-else>- 영업점 문의</span>
+            </li>
+            <li class="mb-4">
+              <h4 class="mb-1">  
               대출 한도
-            </h4> 
-            <span v-html="formattedLoanAmount"></span> <!-- 줄 바꿈 적용 -->
-          </div>
-          <div class="mb-5">
-            <h4>대출 기간</h4> 
-            <span v-html="formattedLoanDuration"></span> <!-- 줄 바꿈 적용 -->
-          </div>
-          <div class="mb-5">
-            <h4>대출 신청 시간</h4> 
-            <span v-html="formattedAvailableHours"></span> <!-- 줄 바꿈 적용 -->
-          </div>
-          <div class="mb-5">
-            <h4>유의사항</h4> 
-              <span v-if="data.loanNotice" v-html="formattedLoanNotice"></span> <!-- 데이터가 있을 때 -->
-              <span v-else>- 대표전화 1588-9999 / 1599-9999 / 1644-9999</span> <!-- 데이터가 없을 때 -->
-            </div>
-            <hr />
+              </h4> 
+              <span v-if="data.loanAmount" v-html="formattedLoanAmount"></span>
+              <span v-else>- 영업점 문의</span>
+            </li>
+            <li class="mb-4">
+              <h4 class="mb-1">대출 기간</h4> 
+              <span v-if="data.loanDurationAndRepayment" v-html="formattedLoanDuration"></span>
+              <span v-else>- 영업점 문의</span>
+            </li>
+            <li class="mb-4">
+              <h4 class="mb-1">대출 신청 시간</h4> 
+              <span v-if="data.availableHours" v-html="formattedAvailableHours"></span>
+              <span v-else>- 영업점 문의</span>
+            </li>
+            <li class="mb-4">
+              <h4 class="mb-1">유의사항</h4> 
+                <span v-if="data.loanNotice" v-html="formattedLoanNotice"></span> <!-- 데이터가 있을 때 -->
+                <span v-else>- 대표전화 1588-9999 / 1599-9999 / 1644-9999</span> <!-- 데이터가 없을 때 -->
+            </li>
+          </ul>
           </div>
         </div>
-        <div class="text-center">
-          <RouterLink :to="`/infoPlaza/KBLoan/`" class="btn btn-sm btn-neutral mb-5 mt-1">목록</RouterLink>
-        </div>      
-    </div>
+        <!-- Sticky sidebar-->
+        <aside class="col-lg-5" style="margin-top: -6rem">
+          <div class="sticky-top" style="padding-top: 6rem">
+            <div class="card shadow-sm mb-3 mb-lg-0">
+              <div class="card-body">
+                <h3>대출 계산기</h3>
+                <p>이 상품에 대한 대출 금액과 이자를 계산하세요!</p>
+                <div class="btn-group" role="group" aria-label="Basic example">
+                  <button
+                    type="button"
+                    class="btn btn-custom"
+                    :class="{ active: activeButton === 'left' }"
+                    @click="setActiveButton('left')"
+                  >
+                    원리금균등
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-custom"
+                    :class="{ active: activeButton === 'middle' }"
+                    @click="setActiveButton('middle')"
+                  >
+                    원금균등
+                  </button>
+                  <button
+                    type="button"
+                    class="btn btn-custom"
+                    :class="{ active: activeButton === 'right' }"
+                    @click="setActiveButton('right')"
+                  >
+                    만기일시
+                  </button>
+                </div>
+                <form @submit.prevent="calculate">
+                  <div class="form-group">
+                    <label for="loanAmount">대출금액</label>
+                    <div class="input-group mb-3">
+                      <input
+                        type="text"
+                        class="form-control"
+                        id="loanAmount"
+                        placeholder="대출금액을 입력하세요"
+                        aria-label="대출금액"
+                        v-model="formattedLoanAmount"
+                        @input="formatCurrency"
+                        required
+                      />
+                      <div class="input-group-append">
+                        <span class="input-group-text">₩</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="form-group">
+                    <label for="loanTerm">대출 기간</label>
+                    <div class="input-group mb-3">
+                      <input
+                        type="number"
+                        class="form-control"
+                        id="loanTerm"
+                        placeholder="대출 기간을 입력하세요"
+                        v-model="loanTerm"
+                        required
+                      />
+                      <span class="input-group-text">개월</span>
+                    </div>
+                  </div>
+                  <div class="d-flex justify-content-end">
+                    <button type="submit" class="btn btn-primary mt-2">
+                      계산하기
+                    </button>
+                  </div>
+                </form>
+                <hr class="my-4" />
+                <div class="d-flex align-items-end">
+                  <div v-if="monthlyPayment || totalInterest" class="result">
+                    <p>월 상환액: {{ monthlyPayment.toFixed(0) }}원</p>
+                    <p>총 이자액: {{ totalInterest.toFixed(0) }}원</p>
+                  </div>
+                  <a
+                    class="btn btn-icon btn-translucent-primary btn-xs rounded-circle"
+                    href="#"
+                    ><i class="fi-telegram"></i
+                  ></a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </aside>
+      </div>
+      <div class="text-center mt-10 mb-5">
+        <RouterLink :to="`/infoPlaza/KBLoan/`" class="btn btn-sm btn-neutral mb-5 mt-1">목록</RouterLink>
+      </div>      
+      <div class="row mt-5">
+        <h3>이 대출 상품이 마음에 드시나요?</h3>
+        <div class="row mt-5">
+          <!-- 카드 여러 개 -->
+          <swiper
+            :slides-per-view="3"
+            :space-between="50"
+            :navigation="true"
+            :pagination="false"
+            :modules="modules"
+            :preventClicks="false"
+            :preventClicksPropagation="false"
+            style="padding-left: 70px; padding-right: 70px"
+          >
+            <swiper-slide v-for="(item, index) in recommandList" :key="index">
+              <div @click="navigateToDetail(item.id)">
+                <RouterLink
+                  :to="`/infoPlaza/jeonseLoan/jeonseLoanDetail/${item.id}`"
+                >
+                  <div class="card card-xl-stretch h-100">
+                    <div
+                      class="card-body pt-5 d-flex flex-column justify-content-between"
+                    >
+                      <div>
+                        <div class="d-flex flex-stack flex-wrap">
+                          <span
+                            class="fs-3 text-gray-500 pe-2"
+                            style="font-weight: 500"
+                          >
+                            <div class="row mb-3" style="height: 70px">
+                              <div class="col-4 d-flex align-items-center">
+                                <img
+                                  :src="
+                                    '/images/banklogo/국민은행.png'
+                                  "
+                                  alt=""
+                                  style="width: 100px"
+                                />
+                              </div>
+                              <div class="col-8 d-flex align-items-center">
+                                <!-- Change align-items to center -->
+                                <span
+                                  class="fs-3 text-gray-500 fw-bolder pe-2 text-left text-truncate"
+                                >
+                                </span>
+                              </div>
+                            </div>
+                            <div class="d-flex flex-stack flex-wrap">
+                              <span
+                                class="fs-3 text-gray-500 pe-2 text-truncate"
+                                style="font-weight: 500"
+                              >
+                              </span>
+                            </div>
+                          </span>
+                        </div>
+                        <div class="carousel-inner pt-6">
+                          <div class="carousel-item active">
+                            <div class="carousel-wrapper">
+                              <div class="d-flex flex-column flex-grow-1">
+                                <!-- 콘텐츠 추가 가능 -->
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                      <!-- 금리 -->
+                      <div
+                        class="d-flex justify-content-end align-items-end ms-auto"
+                      >
+                        <p class="fs-6 text-gray-600 mb-1">금리</p>
+                        <p class="h2 fw-bold mb-0 ms-2">
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </RouterLink>
+              </div>
+            </swiper-slide>
+          </swiper>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -130,4 +308,9 @@ onMounted(async () => {
 
 </script>
 
-<style></style>
+<style>
+.custom-container {
+  max-width: 1100px; /* 원하는 너비로 설정 */
+  margin: 0 auto; /* 가운데 정렬 */
+}
+</style>
