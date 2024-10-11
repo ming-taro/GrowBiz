856bc9db0fa27ee35d700d725393e56e2db48548
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-10-11 16:01:25 +0900

    feat: 커뮤니티 페이지네이션

--- Frontend/src/pages/community/BoardList.vue
+++ Frontend/src/pages/community/BoardList.vue
@@ -120,20 +120,94 @@
       </div>
       
       <!-- 페이지네이션 기능 -->
-      <div v-if="showPagination" class="py-4 px-6 d-flex justify-content-center">
-        <nav aria-label="Page navigation example">
-          <ul class="pagination pagination-spaced gap-1">
-            <li class="page-item" :class="{ disabled: currentPage === 1 }">
-              <a class="page-link" @click="changePage(currentPage - 1)" href="#"><</a>
-            </li>
-            <li class="page-item" v-for="page in totalPages" :key="page" :class="{ active: currentPage === page }">
-              <a class="page-link" @click="changePage(page)" href="#">{{ page }}</a>
-            </li>
-            <li class="page-item" :class="{ disabled: currentPage === totalPages }">
-              <a class="page-link" @click="changePage(currentPage + 1)" href="#">></a>
-            </li>
-          </ul>
-        </nav>
+      <div v-if="showPagination" class="py-4 px-6 mt-3">
+        <div
+          class="row align-items-center justify-content-center text-center"
+        >
+          <div class="col-md-12 d-flex flex-column align-items-center">
+            <!-- Pagination -->
+            <nav aria-label="Page navigation example">
+              <ul class="pagination pagination-spaced gap-1">
+                <!-- First Page Button -->
+                <li class="page-item"
+                  :class="{ disabled: currentPage === 1 }">
+                  <a
+                    class="page-link"
+                    href="#"
+                    @click.prevent="changePage(1)"
+                  >
+                    <i class="fa-solid fa-angles-left"></i>
+                  </a>
+                </li>
+                <!-- Previous Page Button -->
+                <li
+                  class="page-item"
+                  :class="{ disabled: currentPage === 1 }"
+                >
+                  <a
+                    class="page-link"
+                    href="#"
+                    @click.prevent="changePage(currentPage - 1)"
+                  >
+                    <i class="fa-solid fa-angle-left"></i>
+                  </a>
+                </li>
+                <!-- Ellipsis -->
+                <li v-if="currentPage >= 7" class="page-item disabled">
+                  <span class="page-link">...</span>
+                </li>
+                <!-- Page Numbers -->
+                <li
+                  v-for="page in totalPages"
+                  :key="page"
+                  class="page-item"
+                  :class="{ active: currentPage === page }"
+                >
+                  <a
+                    class="page-link"
+                    href="#"
+                    @click.prevent="changePage(page)"
+                  >
+                    {{ page }}
+                  </a>
+                </li>
+                <!-- Ellipsis -->
+                <li
+                  v-if="currentPage <= totalPages - 10"
+                  class="page-item disabled"
+                >
+                  <span class="page-link">...</span>
+                </li>
+                <!-- Next Page Button -->
+                <li
+                  class="page-item"
+                  :class="{ disabled: currentPage === totalPages }"
+                >
+                  <a
+                    class="page-link"
+                    href="#"
+                    @click.prevent="changePage(currentPage + 1)"
+                  >
+                    <i class="fa-solid fa-angle-right"></i>
+                  </a>
+                </li>
+                <!-- Last Page Button -->
+                <li
+                  class="page-item"
+                  :class="{ disabled: currentPage === totalPages }"
+                >
+                  <a
+                    class="page-link"
+                    href="#"
+                    @click.prevent="changePage(totalPages)"
+                  >
+                    <i class="fa-solid fa-angles-right"></i>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
       </div>
     </div>
 </div>
