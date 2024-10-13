e8ce57df88e36969dca7e4501b00a1f788f4acfa
M2nzy <36951721+M2nzy@users.noreply.github.com>
2024-10-13 20:51:55 +0900

    feat: 리포트 상단 그래프 부분 데이터 연결

--- Frontend/src/assets/js/reportChart1.js
+++ Frontend/src/assets/js/reportChart1.js
@@ -1,19 +1,6 @@
 // 위에 그래프 업종 밀도 평균, 업종 평균 가맹비, 업종 인테리어 총 비용
-export const data_bar = {
-    labels: ['Category 1', 'Category 2', 'Category 3'],
-    datasets: [
-      {
-        label: '업종 평균',
-        backgroundColor: '#42a5f5',
-        data: [-20, -30, -40], // 음수 값으로 설정하여 왼쪽으로 그려지도록 설정
-      },
-      {
-        label: '추천 브랜드 평균',
-        backgroundColor: '#ff7043',
-        data: [20, 30, 40], // 양수 값으로 설정하여 오른쪽으로 그려지도록 설정
-      },
-    ],
-  };
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
   
   export const barOptions = {
     responsive: true,
@@ -27,6 +14,9 @@ export const data_bar = {
         },
         ticks: {
           beginAtZero: true, // x축이 0에서 시작되도록 설정
+          callback: function(value) {
+            return ''; // 아래 단위 제거
+          },
         },
       },
       y: {
@@ -37,11 +27,27 @@ export const data_bar = {
       },
     },
     plugins: {
-      legend: {
-        display: true, // 레전드 표시
-        position: 'top', // 레전드 위치 설정 (top, bottom, left, right)
-      },
+        legend: {
+          display: true,
+          position: 'top',
+        },
+        datalabels: {
+          display: true,
+          anchor: 'end',
+          align: 'end',
+          color: '#000',
+          formatter: (value) => {
+            // 음수 값을 양수로 표시
+            return Math.abs(value); // 데이터 값을 양수로 표시
+          },
+        },
     },
+    elements: {
+        bar: {
+          // 막대의 두께를 설정
+          barThickness: 50, // 막대의 두께 (픽셀 단위)
+        },
+      },
   };
   
     
--- Frontend/src/components/report/Bar.vue
+++ Frontend/src/components/report/Bar.vue
@@ -2,28 +2,28 @@
   <div style="background-color: #f6f4f9">
     <div class="container mw-screen-xl">
       <div class="row">
-        <div class="col-6">
-          <div>
-        <h4>추천 브랜드</h4>
-      <span>ㅇㅇ치킨</span>
-      </div>
-      <div>
-        <h4>추천 점수</h4>
-      <span>ㅇㅇ점</span>
-      <span>/ 지역 평균 ㅇㅇ점</span>
+          <div class="col-6 mt-6">
+            <div class="mb-5">
+          <h4 class="mb-1">추천 브랜드</h4>
+          <h2>{{ recommendedBrand }}</h2>
+        </div>
+        <div class="mb-5">
+          <h4 class="mb-1">추천 점수</h4>
+          <h2 class="mb-1">{{ recommendedScore }} 점</h2>
+          <span>/ 지역 평균 {{ industryAverageScore }}</span>
 
+        </div>
+        <div class="mb-5">
+          <h4 class="mb-1">저희가 추천한 브랜드로 창업하실 경우</h4>
+          <h2 class="mb-1">{{ recommendedCost }} 만원</h2> 
+          <h4>만큼 아낄 수 있어요.</h4>
+        </div>
       </div>
-      <div>
-        <h4>저희가 추천한 브랜드로 창업하실 경우</h4>
-      <span>ㅇㅇㅇ만원</span>
-      <span>만큼 아끼실 수 있어요.</span>
-      </div>
-    </div>
 
     <div class="col-6">
       <div class="chart-container">
             <div class="p-5">
-              <canvas id="bar-chart" style="height: 200px"></canvas>
+              <canvas id="bar-chart" style="height: 300px"></canvas>
             </div>
           </div>
       </div>
@@ -35,13 +35,42 @@
 import { onMounted, ref } from 'vue';
 import { Chart } from 'chart.js/auto';
 import {
-  data_bar,
-  barOptions,
+  barOptions
 } from '@/assets/js/reportChart1.js';
+import axios from 'axios';
 
 let barChartInstance1 = ref(null);
+const recommendedBrand = ref('');
+const recommendedScore = ref('');
+const industryAverageScore = ref('');
+const recommendedCost = ref('');
 
-onMounted(() => {
+
+onMounted(async () => {
+  const response = await axios.get('http://localhost:8080/api/report/670a117bf2faf8abef449573');
+  const data = response.data;
+  console.log(data);
+ // 데이터를 차트에 넣기
+ const data_bar = {
+      labels: ['밀도 (단위 : km²)', '가맹비 (단위 : 만원)','총 인테리어 비용 (단위 : 만원)'],
+      datasets: [
+        {
+          label: '업종 평균',
+          backgroundColor: '#fca3b9',
+          data: [
+        data.industry_density_average, // 업종 밀도 평균
+        data.industry_initial_cost, // 업종 평균 가맹비
+        data.industry_total_interior_cost // 업종 총 인테리어 비용
+      ], 
+          },
+        {
+          label: '추천 브랜드 평균',
+          backgroundColor: '#fcd752',
+          data: [data.recommended_brand_density, data.recommended_brand_initial_cost, data.recommended_brand_total_interior_cost], // 추천 브랜드 평균 데이터
+        },
+      ],
+    };
+
   const barCtx = document.getElementById('bar-chart').getContext('2d');
 
   // 첫 번째 가로 양방향 막대 차트
@@ -50,6 +79,15 @@ onMounted(() => {
     data: data_bar,
     options: barOptions,
   }); 
+
+   // 브랜드와 점수 데이터 업데이트
+    recommendedBrand.value = data.brand_name; // 추천 브랜드 이름 추가
+    recommendedScore.value = data.franchise_score; // 추천 점수 추가
+    industryAverageScore.value = data.average_brand_score; // 지역 평균 점수 추가
+    recommendedCost.value = data.recommended_brand_initial_cost; // 추천 비용 추가
+
+
+
 });
 
 </script>
