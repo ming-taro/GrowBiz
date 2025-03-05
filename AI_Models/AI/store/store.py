d8f4792890473c230bb3f05b853476f1eefc6869
ming-taro <hera002008@naver.com>
2025-03-05 15:35:35 +0900

    feat: 구별 가게 밀도 redis 캐시 적용

--- AI_Models/AI/store/store.py
+++ AI_Models/AI/store/store.py
@@ -1,33 +1,59 @@
 import os
 import requests
 from dotenv import load_dotenv
+import redis
 
 load_dotenv()
+
 KAKAO_API_KEY = os.getenv("KAKAO_API_KEY")
-KAKAO_STORE_SEARCH_QUERY = "https://dapi.kakao.com/v2/local/search/keyword.json?query="
+
+# redis
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PORT = os.getenv('REDIS_PORT')
+REDIS_PASSWORD = os.getenv('REDIS_PASSWORD')
+redis_client = redis.Redis(host=REDIS_HOST, port=REDIS_PORT, db=0, password=REDIS_PASSWORD)
+
+KAKAO_SEARCH_REQUEST_URL = "https://dapi.kakao.com/v2/local/search/keyword.json?query="
 
 
 # 카카오 맵 API로 가게 검색
 def count_store_by_location_with_kakao(gu, dong_list, store_name):
+    store_count = 0
+
+    for dong in dong_list:
+        name = f"{gu}:{dong}"
+        key = store_name
+
+        if not redis_client.hexists(name, key):
+            count, api_status = count_store_by_new_location(gu, dong, store_name)
+            if api_status:
+                redis_client.hset(name, key, count)
+                store_count += count
+        else:
+            store_count += int(redis_client.hget(name, key))
+
+    return store_count, True
+
+
+def count_store_by_new_location(gu, dong, store_name):
     headers = {"Authorization": f"KakaoAK {KAKAO_API_KEY}"}
     count = 0
     api_status = True
 
-    for dong in dong_list:
-        query = f"{gu} {dong} {store_name}"
-        url = f"{KAKAO_STORE_SEARCH_QUERY}{query}"
+    query = f"{gu} {dong} {store_name}"
+    url = f"{KAKAO_SEARCH_REQUEST_URL}{query}"
 
-        try:
-            response = requests.get(url, headers=headers)
-            response.raise_for_status()
+    try:
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
 
-            places = response.json().get('documents', [])
-            count += len(places)
-        except requests.exceptions.HTTPError as http_err:
-            print(f"[Kakao Search API] HTTPError {http_err}")
-            api_status = False
-        except requests.exceptions.RequestException as err:
-            print(f"[Kakao Search API] RequestException {err}")
-            api_status = False
+        places = response.json().get('documents', [])
+        count += len(places)
+    except requests.exceptions.HTTPError as http_err:
+        print(f"[Kakao Search API] HTTPError {http_err}")
+        api_status = False
+    except requests.exceptions.RequestException as err:
+        print(f"[Kakao Search API] RequestException {err}")
+        api_status = False
 
     return count, api_status
